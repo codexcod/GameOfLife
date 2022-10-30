118bd0560b4b62d5ae32f212105676461a76d743
Charlie Nunez <nunezr@gmail.com>
2022-10-30 18:33:13 -0300

    Termino de configurar el server

--- alu/server.cpp
+++ alu/server.cpp
@@ -5,14 +5,47 @@ using namespace std;
 // compartir variables y que por lo tanto las herramientas de sincronziacion
 // como semaforos son perfectamente validas.
 
-vector<vector<int>> matrix(3, vector<int>(3));
+vector<vector<int>> socketsClientes(3, vector<int>(3));
+vector<vector<int>> puertosClientes(3, vector<int>(3));
 vector<int> socketsListos
+
+vector<vector<int>> vecinosPosibles{vector<int>{1, 1},
+vector<int>{0, 1},
+vector<int>{-1, 1},
+vector<int>{1, -1},
+vector<int>{1, 0},
+vector<int>{-1, 0},
+vector<int>{0, -1},
+vector<int>{-1, -1}};
+/*
+vecinosPosibles.push_back(vector<int>{1, 1});
+vecinosPosibles.push_back(vector<int>{0, 1});
+vecinosPosibles.push_back(vector<int>{-1, 1});
+vecinosPosibles.push_back(vector<int>{1, 0});
+vecinosPosibles.push_back(vector<int>{-1, 0});
+vecinosPosibles.push_back(vector<int>{1, -1});
+vecinosPosibles.push_back(vector<int>{0, -1});
+vecinosPosibles.push_back(vector<int>{-1, -1});
+*/
 
 
 // Servicio draw: En cada tick, imprime el mapa con el estado de cada celula 
 void draw()
 {
-    /* TO DO*/
+    string tablero = "";
+	for (size_t i = 0; i < socketsClientes.size(); i++)
+    {
+		tablero+= "\n";
+        for (size_t j = 0; j < socketsClientes.size(); j++)
+        {
+			request req;
+            get_request(socketsClientes[i][j], &req);
+			tablero+= " ";
+			tablero+= req.msg;
+			tablero+= " ";
+        }   
+    }
+	cout << tablero << endl;
 }
 
 // Servicio timer: Cada cierto intervalo de tiempo publica un tick. 
@@ -20,10 +53,22 @@ void draw()
 
 void timer()
 {
-   /* TO DO*/
+	int contador = 0;
+
+	while (1)
+	{
+		draw();
+		string tick = "Tiempo " + to_string(contador);
+		char tiempo[tick.length() + 1];
+		strcpy(tiempo, tick.c_str());
+		request req;
+		strncpy(req.msg, tiempo, sizeof(tiempo));
+		strncpy(req.type, "TICK", 10);
+		broadcast(socketsClientes, &req);
+		contador++;
+		sleep(5);
+	}
 }
-
-
 
 // Thread map_creator: Agrega los nuevos nodos al mapa
 void map_creator(/* TO DO*/)
@@ -46,20 +91,75 @@ void map_creator(/* TO DO*/)
 
 }
 
+//Se agrega un nuevo socket hasta alcanzar la cantidad maxima
 void server_accept_conns(int newSocket)
 {
-    socketsListos.push_back(socketNuevo);
+    socketsListos.push_back(newSocket);
     if(socketsListos.size() == 9){
         for (size_t i = 0; i < 3 ; i++)
         {
             for (size_t j = 0; j < 3; j++)
             {
-                matrix[i][j] = socketsListos[i*j];
+                socketsClientes[i][j] = socketsListos[i*j];
                 return true
             }
         }
     }
     return false
+}
+
+//Se calculan las posiciones de los posibles vecinos
+vector<vector<int>> getVecinos(int x, int y)
+{
+	vector<vector<int>> vecinos;
+    for(size_t i = 0; i < 8; i++){
+        calcularUbicacionVecino(x, y, i, vecinos)
+    }
+	return vecinos;
+}
+
+//Se genera el string que funcionara como mensaje para que el cliente conozca sus vecinos
+string stirngVecinos(vector<vector<int>> &vecinos)
+{
+	string vecinosString = "";
+
+	for (int i = 0; i < vecinos.size(); ++i)
+	{
+		vecinosString += " ";
+		vecinosString += to_string(puertosClientes[vecinos[i][0]][vecinos[i][1]]);
+	}
+	return vecinosString;
+}
+
+//Chequea que la posicion sea valida y la agrega al array de vecinos
+int calcularUbicacionVecino(int x, int y, int i, vector<vector<int>> &vecinos)
+{
+        int xVecino = x + vecinosPosibles[i][0];
+        int yVecino = y + vecinosPosibles[i][1];
+        if (xVecino > -1 && yVecino > -1 && xVecino < socketsClientes.size() && yVecino < socketsClientes.size())
+        {
+            vecinos.push_back(vector<int>{xVecino, yVecino});
+        }
+}
+
+//Por cada cliente se recolecta la informacion de sus vecinos y se envia a cada uno
+void notificarClientes()
+{
+	for (size_t i = 0; i < socketsClientes.size(); i++)
+	{
+		for (size_t j = 0; j < socketsClientes.size(); j++)
+		{
+            //Se calcula las posiciones de los vecinos de cada casilla
+			vector<vector<int>> vecinos = getVecinos(i, j, vecinosPosibles, socketsClientes.size());
+            //Se genera un string con los puertos de los clientes vecinos 
+			string vecinos = stirngVecinos(vecinos);
+            //Se envia la info a cada cliente
+			request req;
+			strncpy(req.type, "VECINOS", 8);
+			strncpy(req.msg, vecinos.c_str(), MENSAJE_MAXIMO);
+			send_request(socketsClientes[i][j], &req);
+		}
+	}
 }
 
 int main(void)
@@ -103,10 +203,19 @@ int main(void)
         threads.push_back(thread(connection_handler, socket));
         if (server_accept_conns(socket))
         {
-            request req;
-            strcpy(req.msg, "Todos conectados amiguitos");
-            strcpy(req.type, "Hola \n");
-            broadcast(matrix,&req);
+            for (size_t i = 0; i < socketsClientes.size(); i++)
+			{
+				for (size_t j = 0; j < socketsClientes.size(); j++)
+				{
+					request requestVecino;
+					get_request(socketsClientes[i][j], &requestVecino);
+					char puertas[sizeof(requestVecino.msg)];
+					strncpy(puertas, requestVecino.msg, sizeof(requestVecino.msg));
+					puertosClientes[i][j] = atoi(puertas);
+				}
+			}
+			notificarClientes();
+			threads.push_back(thread(timer, ref(socketsClientes)));
         }
     }
 
